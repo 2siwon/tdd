837afc1e076a6bcb7e985cd6fe7c330ff054c6fa
2siwon <lee89640245@gmail.com>
2017-10-22 23:55:52 +0900

    최소 HTML을 반환하는 기본 뷰

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 home_page = None
 
-def home_page():
-    pass
+
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
